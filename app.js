3f5c61ff161707babbe4f0e18714fdd659ecf31e
jiwonn11 <72331076+jiwonn11@users.noreply.github.com>
2020-10-04 14:48:32 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const addButton = document.querySelector('#add-button');
+
+addButton.addEventListener('click', () => {
+  const input = document.querySelector('#input');
+  const text = input.value.trim();
+
+  if (text !== '') {
+    addToList(text);
+    input.value = '';
+    input.focus();
+    alert("입력이 완료되었습니다");
+  }
+});
+
+function addToList(text) {
+  const list = document.querySelector('#list');
+
+  const newListItem = document.createElement('li');
+  newListItem.classList.add('list-item'); 
+
+  newListItem.innerHTML = text;
+
+  list.appendChild(newListItem);
+}
